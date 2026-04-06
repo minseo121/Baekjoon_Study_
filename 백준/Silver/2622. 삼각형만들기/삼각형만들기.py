a69019ed66c46849df64bef4de6f4b17145558ba
minseo121 <122244556+minseo121@users.noreply.github.com>
2026-04-06 15:40:39 +0900

    [Silver V] Title: 삼각형만들기, Time: 836 ms, Memory: 110736 KB -BaekjoonHub

---
+++ 백준/Silver/2622. 삼각형만들기/삼각형만들기.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input().strip())
+if n == 1 or n == 2 or n==4:
+    print(0)
+else:
+    num = 0
+    for i in range(1, n+1):
+        for j in range(i, n+1):
+            k = n - (i+j)
+            if k >= (i+j):
+                continue
+            else:
+                if j > k:
+                    break
+                num += 1
+    print(num)
